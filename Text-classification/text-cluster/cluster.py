f889490e68e618a15137715293f5ad8adf9bdc03
harry <tangkaiyuvip@gmail.com>
2018-05-30 14:03:15 +0800

    train doc2vec

--- Text-classification/text-cluster/cluster.py
+++ Text-classification/text-cluster/cluster.py
@@ -6,7 +6,9 @@
 # @File    : cluster.py
 
 import json
+import multiprocessing
 import os
+import sys
 from itertools import chain
 
 import numpy as np
@@ -25,11 +27,13 @@ window_size = 20
 min_count = 4
 sampling_threshold = 1e-5
 negative_size = 5
-train_epoch = 30
+train_epoch = 300
 dm = 0  # 0 = dbow; 1 = dmpv
-worker_count = 64  # number of parallel processes
+worker_count = multiprocessing.cpu_count()  # number of parallel processes
 kmeans_clusters = 8
-path_model = ''
+path_model = 'model'
+if not os.path.exists(os.path.join(os.getcwd(), path_model)):
+    os.makedirs(os.path.join(os.getcwd(), path_model))
 
 def read_from_dir(root):
     file_list = os.listdir(root)
@@ -71,6 +75,7 @@ def labelizeData(corpus):
 def get_dataset(root=''):
     print('loading data')
     base_dir = os.getcwd()
+    # print(base_dir)
     if not os.path.exists(os.path.join(base_dir, 'match-data.json')):
         file_dir = '/Users/harry/Downloads/okoo-matches-NoTime'
         data = read_from_dir(file_dir)
@@ -93,12 +98,13 @@ def train_doc2vec(train_data, dim=vector_size, epoch_num=train_epoch, window_siz
                            work=workers, min_count=min_count, epoches=epoch_num)
     else:
         model_dm = Doc2Vec.load(os.path.join(path_model, 'model_dm'))
-    if not os.path.exists(os.path.join(path_model,'model_dbow')) or new_model:
+    if not os.path.exists(os.path.join(path_model, 'model_dbow')) or new_model:
         model_dbow = Doc2Vec(dm=0, vector_size=dim, window=window_size,
                              work=workers, min_count=min_count, epoches=epoch_num)
     else:
         model_dbow = Doc2Vec.load(os.path.join(path_model, 'model_dbow'))
     if not new_model:
+        print('finished train model')
         return model_dm, model_dbow
     # build vocabulary
     model_dm.build_vocab(all_data)
@@ -107,8 +113,8 @@ def train_doc2vec(train_data, dim=vector_size, epoch_num=train_epoch, window_siz
     # train model each epoch permutate the data
     model_dbow.train(all_data, epochs=model_dbow.epochs, total_examples=model_dbow.corpus_count)
     model_dm.train(all_data, epochs=model_dm.epochs, total_examples=model_dm.corpus_count)
-    model_dm.save(os.path.join(path_model,'model_dm'))
-    model_dbow.save(os.path.join('model_dbow'))
+    model_dm.save(os.path.join(path_model, 'model_dm'))
+    model_dbow.save(os.path.join(path_model, 'model_dbow'))
     print('finished train model')
     return model_dm, model_dbow
 
@@ -118,12 +124,13 @@ def get_doc_vec(model, corpus, output_dim=vector_size):
     return np.concatenate(vecs)
 
 
-def Cluster(train_vecs, model_path=None, test_vecs=None):
+def Cluster(train_vecs, model_name=None, test_vecs=None):
     print('training cluster')
     kmeans_model = KMeans(n_clusters=kmeans_clusters, n_jobs=worker_count, )
     kmeans_model.fit(train_vecs)
     labels = kmeans_model.predict(train_vecs)
     cluster_centers = kmeans_model.cluster_centers_
+    #kmeans_model.save(os.path.join(path_model, model_name))
     print('finished cluster')
 
     return labels, cluster_centers
@@ -132,6 +139,7 @@ def Cluster(train_vecs, model_path=None, test_vecs=None):
 def visualize(vecs, labels):
     tsne = TSNE(n_components=2, init='pca', random_state=0)
     X_tsne = tsne.fit(vecs)
+    #tsne.save(os.path.join(path_model, 'tsne.h5'))
     X_tsne = preprocessing.normalize(X_tsne.embedding_, norm='l2')
     fig = plt.figure()
     ax = plt.subplot(111)
@@ -145,11 +153,12 @@ def visualize(vecs, labels):
 
 
 if __name__ == '__main__':
+    train_epoch = sys.argv[1]
     data = get_dataset()
     model_dm, model_dbow = train_doc2vec(data, new_model=True)
-    vecs_dm = get_doc_vec(model_dm, data)
-    vecs_dbow = get_doc_vec(model_dbow, data)
-    labels_dm, cluster_centers_dm = Cluster(vecs_dm)
-    labels_dbow, cluster_centers_dbow = Cluster(vecs_dbow)
-    visualize(vecs_dm, labels_dm)
+    #vecs_dm = get_doc_vec(model_dm, data)
+    #vecs_dbow = get_doc_vec(model_dbow, data)
+    #labels_dm, cluster_centers_dm = Cluster(vecs_dm, 'dm.h5')
+    #labels_dbow, cluster_centers_dbow = Cluster(vecs_dbow, 'dbow.h5')
+    #visualize(vecs_dm, labels_dm)
     print()
