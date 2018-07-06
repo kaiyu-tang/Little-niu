624484710a20e1ffa7e708028e76a8cc31a0ab81
harry <tangkaiyuvip@gmail.com>
2018-07-06 16:10:03 +0800

    word2vec

--- Text-classification/text-classify/Config.py
+++ Text-classification/text-classify/Config.py
@@ -37,21 +37,21 @@ class Config:
     word2vec_dm_concat = 0  # very time consuming
     word2vec_net_size = 512
     word2vec_train_epoch = 20
-    word2vec_window = 10
+    word2vec_window = 5
     word2vec_worker = multiprocessing.cpu_count()//3
     word2vec_min_count = 2
     word2vec_sg = 1
-    word2vec_model_name = 'word2vec-skim.pt'
+    word2vec_model_name = 'word2vec-skim-5.pt'
     word2vec_negative = 0
     word2vec_iter = 20
 
     # fast_text
     fast_sg = 1
-    fast_window = 10
+    fast_window = 5
     fast_min_count = 2
     fast_worker = multiprocessing.cpu_count()//3
     fast_iter = 20
-    fast_model_name = 'fasttext-skim.pt'
+    fast_model_name = 'fasttext-skim-5.pt'
     # wordrank
     wordrank_window = 15
     wordrank_symmetric = 1
