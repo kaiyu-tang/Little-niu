99d1fb4a6917094ae5838d5601e256e147860458
harry <tangkaiyuvip@gmail.com>
2018-07-20 16:46:36 +0800

    word2vec

---
+++ Text_classification/text_classify/data/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python  
+# -*- coding: utf-8 -*-  
+# @Time    : 2018/7/17 下午3:13  
+# @Author  : Kaiyu  
+# @Site    :   
+# @File    : __init__.py
---
+++ ocr/src/ocr.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python  
+# -*- coding: utf-8 -*-  
+# @Time    : 2018/7/17 下午2:23  
+# @Author  : Kaiyu  
+# @Site    :   
+# @File    : ocr.py
